54fa359ddf745b6e2d68874e4f212e77868d419e
beingritik <ritikjain98.r@gmail.com>
2022-09-30 10:21:05 +0530

    ritik modules added

--- app.js
+++ app.js
@@ -11,9 +11,14 @@ app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
 
 app.set("view engine", "ejs");
+// app.get("/", function(req, res){
+//     res.render('index')
+// })
 
-app.use('/css', express.static(path.join(__dirname, "assets/css")));
+app.use('/css', express.static(path.join(__dirname, "public/css")));
 app.use('/css', express.static(path.join(__dirname, "/node_modules/bootstrap/dist/css")));
+app.use('/images', express.static(path.join(__dirname, "public/images")));
+// app.use('/js', express.static(path.join(__dirname, "public/js")));
 
 app.use('/', require('./routes/router'));
 
--- service/render.js
+++ service/render.js
@@ -2,6 +2,6 @@
 var mongoose = require('mongoose');
 const userModel = require('../db/users');
 exports.homeroute = (req, res) => {
-    res.render('./index');
+    res.render('index');
 }
 
